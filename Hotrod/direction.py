fa4fee2e8c44ba12373e856b42ce32cb314da60c
NecroReindeer <necroreindeer@gmail.com>
2015-11-27 13:31:47 +0000

    Added CellEdge class to store the type of edge
    Added CellEdge that can be a wall or passage.
    Added enum for wall and passage.
    Added method in direction enum for converting directions to angles.

--- Hotrod/direction.py
+++ Hotrod/direction.py
@@ -4,7 +4,17 @@ __author__ = 'Hat'
 from enum import Enum
 
 class Direction(Enum):
-    right = (1, 0)
-    down = (0, -1)
     left = (-1, 0)
+    down = (0, -1)
+    right = (1, 0)
     up = (0, 1)
+
+    def get_angle(self):
+        if self == Direction.left:
+            return 0
+        elif self == Direction.down:
+            return 90
+        elif self == Direction.right:
+            return 180
+        elif self == Direction.up:
+            return 270
--- Hotrod/main.py
+++ Hotrod/main.py
@@ -1,6 +1,7 @@
 __author__ = 'Harriet'
 
 import random
+from enum import Enum
 
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -28,7 +29,10 @@ class PlayArea(Widget):
             for y in range(self.rows):
                self.create_cell((x, y))
         for cell in self.cells:
-            self.add_widget(cell)
+            cell.set_edges()
+            # Add widget at index 1 so that PlayerBeetle remains at 0
+            self.add_widget(cell, 1)
+
         self.player.size = self.cells[0].interior
         self.player.center = self.cells[0].center
 
@@ -45,66 +49,93 @@ class PlayArea(Widget):
 
     def update(self):
         self.player.move()
-        #
-        # for cell in self.cells:
-        #     for wall in cell.walls:
-        #         if wall.collide_widget(self.player):
-        #             self.player.velocity = 0, 0
 
         if (self.player.y <= 0 or self.player.top >= self.top):
-            self.player.velocity_y = 0
+            self.player.can_move = False
         if (self.player.x <= self.x or self.player.right >= self.right):
-            self.player.velocity_x = 0
+            self.player.can_move = False
 
     def on_touch_up(self, touch):
         # Move right if player swipes right
         if touch.pos[0] > touch.opos[0] + self.width/10:
+            self.player.can_move = True
             self.player.move_direction = direction.Direction.right
         # Move left if player swipes left
         if touch.pos[0] < touch.opos[0] - self.width/10:
+            self.player.can_move = True
             self.player.move_direction = direction.Direction.left
         # Move up is player swipes up
         if touch.pos[1] > touch.opos[1] + self.height/10:
+            self.player.can_move = True
             self.player.move_direction = direction.Direction.up
         # Move down if player swipes down
         if touch.pos[1] < touch.opos[1] - self.height/10:
+            self.player.can_move = True
             self.player.move_direction = direction.Direction.down
         self.player.color = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
 
 
 class Wall(Widget):
     angle = NumericProperty(0)
+    origin = ObjectProperty((0,0))
 
+class CellEdgeType(Enum):
+    passage = 0
+    wall = 1
+
+
+class CellEdge(Widget):
+    type = ObjectProperty(CellEdgeType.wall)
+    direction = ObjectProperty(direction.Direction.right)
+
+    def set_edge(self):
+        """Check if the edge should be a wall or a passage"""
+        if self.type == CellEdgeType.wall:
+            wall = Wall()
+            wall.size = self.size
+            wall.pos = self.pos
+            wall.origin = self.parent.center
+            wall.angle = self.direction.get_angle()
+            self.add_widget(wall)
+        elif self.type == CellEdgeType.passage:
+            pass
+
+    def get_edge(self):
+        return self.type
 
 class Cell(Widget):
     sides = NumericProperty(4)
     wall_thickness = NumericProperty(0.1)
     interior = ListProperty()
 
-    left_wall = ObjectProperty(None)
-    right_wall = ObjectProperty(None)
-    top_wall = ObjectProperty(None)
-    bottom_wall = ObjectProperty(None)
+    left_edge = ObjectProperty(None)
+    right_edge = ObjectProperty(None)
+    top_edge = ObjectProperty(None)
+    bottom_edge = ObjectProperty(None)
 
-    walls = ListProperty()
+    edges = ListProperty()
+
+    def set_edges(self):
+        for edge in self.edges:
+            edge.set_edge()
 
 
 class PlayerBeetle(Widget):
-    speed = NumericProperty()
-    color = ObjectProperty((1, 1, 1))
+    speed = NumericProperty(5)
+    color = ObjectProperty((1, 0, 1))
 
     move_direction = ObjectProperty(direction.Direction.right)
 
-    can_move = BooleanProperty(True)
+    can_move = BooleanProperty(False)
 
     def move(self):
-        self.pos = Vector(self.move_direction.value[0] * self.speed,
-                          self.move_direction.value[1] * self.speed) + self.pos
+        if self.can_move:
+            self.pos = Vector(self.move_direction.value[0] * self.speed,
+                              self.move_direction.value[1] * self.speed) + self.pos
 
 
 class HotrodGame(Widget):
     play_area = ObjectProperty(None)
-
 
     def start(self):
         self.play_area.generate_level()
